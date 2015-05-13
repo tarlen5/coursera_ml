94e0a9bb79f07fa707843040b9c48a19691fc0f7
Tim Arlen (Office) <tca3@psu.edu>
2015-05-13 15:04:50 -0400

    Finished ex5

--- unit10/ex5/validationCurve.m
+++ unit10/ex5/validationCurve.m
@@ -9,45 +9,27 @@ function [lambda_vec, error_train, error_val] = ...
 %       y) and validation set (Xval, yval).
 %
 
-% Selected values of lambda (you should not change this)
-lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
-
-% You need to return these variables correctly.
-error_train = zeros(length(lambda_vec), 1);
-error_val = zeros(length(lambda_vec), 1);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the validation errors in error_val. The 
-%               vector lambda_vec contains the different lambda parameters 
-%               to use for each calculation of the errors, i.e, 
-%               error_train(i), and error_val(i) should give 
-%               you the errors obtained after training with 
-%               lambda = lambda_vec(i)
-%
-% Note: You can loop over lambda_vec with the following:
-%
-%       for i = 1:length(lambda_vec)
-%           lambda = lambda_vec(i);
-%           % Compute train / val errors when training linear 
-%           % regression with regularization parameter lambda
-%           % You should store the result in error_train(i)
-%           % and error_val(i)
-%           ....
-%           
-%       end
-%
-%
-
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
+  % Selected values of lambda (you should not change this)
+  lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+  lambda_train = 0
+  
+  % You need to return these variables correctly.
+  error_train = zeros(length(lambda_vec), 1);
+  error_val = zeros(length(lambda_vec), 1);
+  
+  for i = 1:length(lambda_vec)
+    % function [theta] = trainLinearReg(X, y, lambda)
+    % function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+    lambda = lambda_vec(i)
+    [theta] = trainLinearReg(X, y, lambda);
+    
+    [J_train, grad] = linearRegCostFunction(X, y, theta, lambda_train);
+    [J_val, grad] = linearRegCostFunction(Xval, yval, theta, lambda_train);
+    
+    error_train(i) = J_train;
+    error_val(i) = J_val
+    
+  endfor
+  
+  
 end
